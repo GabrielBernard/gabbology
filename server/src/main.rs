c8b5c5ce24c571744a7fa74626fdcf97969203ec
Gabriel Bernard <gabriel.f.bernard@gmail.com>
2023-04-15 20:15:41 +0000

    statically serve nextjs single-page application

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,31 @@
-use axum::{routing::get, Router};
+use axum::Router;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(using_serve_dir_with_assets_fallback().into_make_service())
         .await
         .unwrap();
+}
+
+fn using_serve_dir_with_assets_fallback() -> Router {
+    let not_found = ServeFile::new("../app/.next/server/pages/404.html");
+
+    let serve_next_dir = ServeDir::new("../app/.next");
+
+    let serve_index = ServeDir::new("../app/.next/server/pages")
+        .append_index_html_on_directories(true)
+        .not_found_service(not_found.clone());
+
+    let assets_dir = ServeDir::new("../app/public/assets");
+
+    let favicon = ServeFile::new("../app/public/favicon.ico");
+
+    Router::new()
+        .nest_service("/", serve_index)
+        .nest_service("/assets", assets_dir)
+        .nest_service("/_next", serve_next_dir)
+        .nest_service("/favicon.ico", favicon)
+        .fallback_service(not_found)
 }
