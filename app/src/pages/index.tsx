0d5e0ef81286b3cdab34f86c604f6aa2f5267167
Gabriel Bernard <GabrielBernard@users.noreply.github.com>
2023-04-19 22:04:59 -0400

    update text and styling (#24)
    - add container icon to mobile version
    - update styles
    - change text

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 font-sans">
-      <div className="z-10 flex w-screen justify-end gap-12 p-6 sm:w-full sm:max-w-3xl sm:p-0 md:justify-start">
+    <main className="my-10 flex min-h-screen flex-col items-center px-24 font-sans">
+      <div className="z-10 flex w-screen items-end justify-end gap-4 pr-6 sm:w-full sm:max-w-3xl sm:gap-12 sm:p-0 md:items-start md:justify-start">
         <Image
           src="/assets/containers.svg"
           alt="Containers"
-          className="hidden dark:invert md:block"
+          className="h-full w-24 dark:invert md:h-64 md:w-64"
           width={250}
           height={250}
         />
@@ -106,7 +106,7 @@ export default function Home() {
             className="text-sky-700 hover:underline dark:text-sky-600"
           >
             Let&apos;s Encrypt
-          </a>
+          </a>{" "}
           and is being kept updated by{" "}
           <a
             href="https://cert-manager.io/"
@@ -125,8 +125,15 @@ export default function Home() {
             {" "}
             Kubernetes
           </a>{" "}
-          environment serving an application with an Ingress using a TLS
-          certificate signed by an officially trusted authority, in our case{" "}
+          environment serving an application with an{" "}
+          <a
+            href="https://kubernetes.io/docs/concepts/services-networking/ingress/"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            Ingress
+          </a>{" "}
+          using a TLS certificate signed by an officially trusted authority, in
+          our case{" "}
           <a
             href="https://letsencrypt.org/"
             className="text-sky-700 hover:underline dark:text-sky-600"
@@ -145,14 +152,54 @@ export default function Home() {
           </a>{" "}
           to obtain a certificate signed by a trusted entity in itself was the
           goal of the project, it is now used to host a single-page application
-          served by the cluster. The project will grow and will serve as a
-          showcase of my abilities and knowledge of{" "}
+          served by a server written in{" "}
+          <a
+            href="https://www.rust-lang.org/"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            Rust
+          </a>{" "}
+          with the{" "}
+          <a
+            href="https://github.com/tokio-rs/axum"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            axum crate
+          </a>
+          . The application is packaged in a container image built automatically
+          with{" "}
+          <a
+            href="https://docs.github.com/en/actions"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            GitHub actions
+          </a>
+          . The deployement on the cluster is managed by configurations hosted
+          in{" "}
+          <a
+            href="https://github.com/GabrielBernard/gabbology"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            GitHub
+          </a>{" "}
+          and synchronized with{" "}
+          <a
+            href="https://argoproj.github.io/cd/"
+            className="text-sky-700 hover:underline dark:text-sky-600"
+          >
+            Argo CD
+          </a>
+          .
+        </p>
+        <p className="pb-6">
+          The project will grow and will serve as a showcase of my abilities and
+          knowledge of{" "}
           <a
             href="https://kubernetes.io/"
             className="text-sky-700 hover:underline dark:text-sky-600"
           >
             Kubernetes
-          </a>{" "}
+          </a>
           . All the code used for this project is accessible on my{" "}
           <a
             href="https://github.com/GabrielBernard/gabbology"
@@ -199,7 +246,7 @@ export default function Home() {
             >
               Let&apos;s Encrypt
             </a>{" "}
-            asks for a prof of control with a{" "}
+            asks for a proof of control with a{" "}
             <a
               href="https://letsencrypt.org/docs/challenge-types/#dns-01-challenge"
               className="text-sky-700 hover:underline dark:text-sky-600"
@@ -240,11 +287,16 @@ export default function Home() {
             >
               CloudFlare
             </a>{" "}
-            proxy requests, using the strict trusted certificate policy
+            proxy requests, using the{" "}
+            <a
+              href="https://developers.cloudflare.com/ssl/origin-configuration/ssl-modes/full-strict/"
+              className="text-sky-700 hover:underline dark:text-sky-600"
+            >
+              Full (strict) TLS encryption mode
+            </a>
           </li>
           <li>
-            Client&apos;s communicating with the server can expect full TLS
-            encryption
+            Communication with the server is encrypted with trusted certificates
           </li>
         </ol>
       </div>
