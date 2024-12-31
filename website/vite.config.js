ab344ca56213e34256c969bff11d49ff139424a3
Gabriel Bernard <gabriel.f.bernard@gmail.com>
2024-12-31 17:35:40 +0000

    more tests showing templating is necessary for devx

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,4 +1,27 @@
 /** @type {import('vite').UserConfig} */
 export default {
   publicDir: "assets",
+  build: {
+    rollupOptions: {
+      // Suppress the warning:
+      //
+      // ```
+      // node_modules/htmx.org/dist/htmx.esm.js (854:13):
+      // Use of eval in "node_modules/htmx.org/dist/htmx.esm.js" is strongly discouraged
+      // as it poses security risks and may cause issues with minification.
+      // ```
+      //
+      // See https://github.com/bigskysoftware/htmx/pull/1988#issuecomment-1806290317
+      // for an explanation on why this is not an issue.
+      onwarn: (entry, next) => {
+        if (
+          entry.loc?.file &&
+          /htmx\.esm\.js$/.test(entry.loc.file) &&
+          /Use of eval in/.test(entry.message)
+        )
+          return;
+        return next(entry);
+      },
+    },
+  },
 };
